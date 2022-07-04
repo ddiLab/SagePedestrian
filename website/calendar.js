eb49649757fd57486a0f88411155b4b5a2157eb2
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2022-07-04 13:40:02 -0500

    Now has selectable days, needs a way to get the array of dates from DB

--- website/calendar.js
+++ website/calendar.js
@@ -1,5 +1,30 @@
-$( function(){
+//Thanks to stack overflow for providing a reference on how to create a selectable calendar
+//The skeleton for this page was found at this post on stack overflow
+//https://stackoverflow.com/questions/57582127/highlighting-specific-days-in-jquery-datepicker
 
-    $("#dateinput").datepicker();
 
-});
+$(function() {
+    
+    //need to find a way to get the list of dates from the database
+    var enableDays = ['2022-07-04', '2022-07-05', '2022-07-06'];
+  
+    console.log(enableDays);
+  
+    function enableAllTheseDays(date) {
+      var fDate = $.datepicker.formatDate('yy-mm-dd', date);
+      var result = [false, ""];
+      $.each(enableDays, function(k, d) {
+        if (fDate === d) {
+          result = [true, "highlight-green"];
+        }
+      });
+      return result;
+    }
+  
+    //this section creates the datepicker and gives it the formatting we need, and makes only a select amount of days available
+    $("#dateinput").datepicker({
+      dateFormat: "yy-mm-dd",
+      beforeShowDay: enableAllTheseDays,
+    });
+  
+  });
