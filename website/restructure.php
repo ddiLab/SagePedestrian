ad951b5d36a8adc21df2fdbe91439aac34e1ac1b
Justin Derus <77174869+Justin-Derus@users.noreply.github.com>
2022-08-23 16:03:05 -0500

    Changed the css for calendar and ui stuff

--- website/restructure.php
+++ website/restructure.php
@@ -5,7 +5,12 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Crosswalk Detection</title>
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/Le-Frog/jquery-ui.css">
+        <link rel="stylesheet" href="./jquery-ui-1.13.2.custom/jquery-ui.css">
+        <link rel="stylesheet" href="./jquery-ui-1.13.2.custom/jquery-ui.min.css">
+        <link rel="stylesheet" href="./jquery-ui-1.13.2.custom/jquery-ui.theme.css">
+        <link rel="stylesheet" href="./jquery-ui-1.13.2.custom/jquery-ui.theme.min.css">
+        <link rel="stylesheet" href="./jquery-ui-1.13.2.custom/jquery-ui.structure.css">
+        <link rel="stylesheet" href="./jquery-ui-1.13.2.custom/jquery-ui.structure.min.css">
         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
         <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>    
         <script src="https://code.jquery.com/jquery-1.9.1.js"></script>
